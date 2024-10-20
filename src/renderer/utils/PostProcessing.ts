db05e9f69e7e7b6b8c42f15fbe05f7d5124bc1ed
Björn Hund <bjoern@hund-edv.de>
2024-10-20 08:37:29 +0200

    Fix code layout
    - Remove double spaces

--- src/renderer/utils/PostProcessing.ts
+++ src/renderer/utils/PostProcessing.ts
@@ -4,71 +4,71 @@ import fs from 'fs-extra';
 import defaultConfig from './postProcessConfig.json';
 
 interface PostProcessValue {
-    oldValue: string | RegExp,
-    newValue: string
+  oldValue: string | RegExp;
+  newValue: string;
 }
 
 interface PostProcessFile {
-    fileName: string;
-    values: PostProcessValue | PostProcessValue[]
+  fileName: string;
+  values: PostProcessValue | PostProcessValue[];
 }
 
 interface PostProcessConfiguration {
-    files: PostProcessFile[];
+  files: PostProcessFile[];
 }
 
 export const processFiles = async (addOnKey: string, installDir: string): Promise<void> => {
-    if (addOnKey !== 'A32NX') {
-        return;
-    }
+  if (addOnKey !== 'A32NX') {
+    return;
+  }
 
-    // const configuration = await getConfiguration();
-    const configuration = defaultConfig;
-    configuration?.files?.forEach(async file => {
-        const fileName = path.join(installDir, file.fileName);
-        if (await fs.pathExists(fileName)) {
-            await convertFile(fileName, file.values);
-        }
-    });
+  // const configuration = await getConfiguration();
+  const configuration = defaultConfig;
+  configuration?.files?.forEach(async (file) => {
+    const fileName = path.join(installDir, file.fileName);
+    if (await fs.pathExists(fileName)) {
+      await convertFile(fileName, file.values);
+    }
+  });
 };
 
 const convertFile = async (fileName: string, values: PostProcessValue | PostProcessValue[]): Promise<void> => {
-    let saveChanges: boolean;
-    let content: string = (await fs.readFile(fileName)).toString();
+  let saveChanges: boolean;
+  let content: string = (await fs.readFile(fileName)).toString();
 
-    if (Array.isArray(values)) {
-        values.forEach(async value => {
-            const { oldValue, newValue } = value;
-            content = content.replaceAll(oldValue, newValue);
-            saveChanges = true;
-        });
-    } else {
-        const { oldValue, newValue } = values;
-        content = content.replaceAll(oldValue, newValue);
-        saveChanges = true;
-    }
+  if (Array.isArray(values)) {
+    values.forEach(async (value) => {
+      const { oldValue, newValue } = value;
+      content = content.replaceAll(oldValue, newValue);
+      saveChanges = true;
+    });
+  } else {
+    const { oldValue, newValue } = values;
+    content = content.replaceAll(oldValue, newValue);
+    saveChanges = true;
+  }
 
-    if (saveChanges) {
-        await fs.writeFile(fileName, content);
-    }
+  if (saveChanges) {
+    await fs.writeFile(fileName, content);
+  }
 };
 
 const getConfiguration = async (): Promise<PostProcessConfiguration> => {
-    let configuration: PostProcessConfiguration;
+  let configuration: PostProcessConfiguration;
 
-    const configPath = path.join(process.env.APPDATA, 'FlyByWire Installer');
-    const configFile = path.join(configPath, 'postProcessConfig.json');
+  const configPath = path.join(process.env.APPDATA, 'FlyByWire Installer');
+  const configFile = path.join(configPath, 'postProcessConfig.json');
 
-    if (!await fs.pathExists(configPath)) {
-        await fs.mkdirp(configPath);
-    }
+  if (!(await fs.pathExists(configPath))) {
+    await fs.mkdirp(configPath);
+  }
 
-    if (await fs.pathExists(configFile)) {
-        configuration = JSON.parse((await fs.readFile(configFile)).toString());
-    } else {
-        configuration = defaultConfig;
-        await fs.writeFile(configFile, JSON.stringify(configuration));
-    }
+  if (await fs.pathExists(configFile)) {
+    configuration = JSON.parse((await fs.readFile(configFile)).toString());
+  } else {
+    configuration = defaultConfig;
+    await fs.writeFile(configFile, JSON.stringify(configuration));
+  }
 
-    return configuration;
+  return configuration;
 };
